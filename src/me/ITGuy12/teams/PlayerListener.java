4acb308593f5656bc58123aa0c5ce495585b0f2b
JavaGuy12 <itguy12games@gmail.com>
2017-12-24 12:56:59 -0500

    Added player leave stuff.
    When a player leaves, now, they will be removed from their team.

--- src/me/ITGuy12/teams/PlayerListener.java
+++ src/me/ITGuy12/teams/PlayerListener.java
@@ -5,6 +5,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener{
 
@@ -20,6 +22,42 @@ public class PlayerListener implements Listener{
 					damager.sendMessage(Main.prefix + ChatColor.RED + "ERROR: You cannot attack teammates!");
 				}
 			}
+		}
+	}
+	
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent e) {
+		if(TeamManager.getTeamManager().isInATeam(e.getPlayer())) {
+			if(TeamManager.getTeamManager().getTeamForMember(e.getPlayer()).getLeader().getName() == e.getPlayer().getName()){
+				TeamManager.getTeamManager().removeTeam(TeamManager.getTeamManager().getTeamForMember(e.getPlayer()));
+				for (Player p : TeamManager.getTeamManager().getTeamForMember(e.getPlayer()).getTeamMembersArray()) {
+					p.sendMessage(Main.prefix + ChatColor.RED + e.getPlayer().getDisplayName() + ChatColor.RED + " has left, and therefore your team has been disbanded.");
+				}
+				return;
+			}
+			
+			Team t = TeamManager.getTeamManager().getTeamForMember(e.getPlayer());
+			
+			t.removePlayer(e.getPlayer());
+			
+		}
+	}
+	
+	public void onPlayerKicked(PlayerKickEvent e) {
+		if(TeamManager.getTeamManager().isInATeam(e.getPlayer())) {
+			if(TeamManager.getTeamManager().getTeamForMember(e.getPlayer()).getLeader().getName() == e.getPlayer().getName()){
+				TeamManager.getTeamManager().removeTeam(TeamManager.getTeamManager().getTeamForMember(e.getPlayer()));
+				for (Player p : TeamManager.getTeamManager().getTeamForMember(e.getPlayer()).getTeamMembersArray()) {
+					p.sendMessage(Main.prefix + ChatColor.RED + e.getPlayer().getDisplayName() + ChatColor.RED + " has left, and therefore your team has been disbanded.");
+				}
+				return;
+			}
+			
+			Team t = TeamManager.getTeamManager().getTeamForMember(e.getPlayer());
+			
+			t.removePlayer(e.getPlayer());
+			
 		}
 	}
 }
